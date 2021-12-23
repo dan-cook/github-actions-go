403bfed003f2a3e7eb8397ee4a5a4b477c7b81c2
Dan Cook <dancook.developer+github@gmail.com>
2021-12-23 16:59:58 +0000

    adds handler tests

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -9,10 +9,21 @@ var _ http.Handler = &PingHandler{}
 
 type PingHandler struct{}
 
+func NewPingHandler() *PingHandler {
+	return &PingHandler{}
+}
+
 // Ping returns a Pong!
 func (h *PingHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	w.Header().Set("Content-Type", "text/plain")
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
 
+	h.serve(w, r)
+}
+
+func (h *PingHandler) serve(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
 	fmt.Fprint(w, "Pong!")
 }
